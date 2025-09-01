bb73ead15c5dc481322ee83323bc9c5e5486dbd4
Arturo4102 <arturoarellano4102@gmail.com>
2025-09-01 10:38:12 +0200

    fix: update base URL configuration for GitHub Pages and improve Open Graph image path handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,10 @@ import tailwind from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://arturo4102.github.io",   // <- dominio base
-  base: "/Curriculum_Vitae/",             // <- subcarpeta del repo
+  site: "https://arturo4102.github.io",
+  base: "/Curriculum_Astro/",
   output: "static",
-  vite:{
-    plugins:[tailwind()]
-  },
-  integrations: [react(), mdx(), sitemap()]
+  build: { inlineStylesheets: "always" },
+  vite: { plugins: [tailwind()] },
+  integrations: [react(), mdx(), sitemap()],
 });
